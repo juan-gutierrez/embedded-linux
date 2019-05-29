e2e02a4fd6fa8ecb76f2c1f00dd05784a94dfa24
Juan Gutierrez <juan.gutierrez@nxp.com>
2019-05-29 18:49:16 -0500

    scripts: add script directory to the path
    Add script directory to the path so other scripts called within
    ireplace.sh can be reached.
    
    Signed-off-by: Juan Gutierrez <juan.gutierrez@nxp.com>

--- scripts/ireplace.sh
+++ scripts/ireplace.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 
+export PATH=$PATH:$(dirname $(realpath $0))
+
 read -p "find what: " usrInput
 from=$usrInput
 read -p "replace with: " usrInput
@@ -9,6 +11,6 @@ filename=$usrInput
 
 echo "replacing $from with $to on file: $filename"
 
-./replace.sh "$from" "$to" "$filename"
+replace.sh "$from" "$to" "$filename"
 
 
